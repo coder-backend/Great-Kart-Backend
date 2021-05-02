564abe2f225522b62048557b64ecc1557b5a8a70
ashish807 <ashish.adhikari727@gmail.com>
2021-05-02 13:29:12 +0545

    context processors & product details

--- greatKart/store/models.py
+++ greatKart/store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from category.models import Category
+from django.urls import reverse
 # Create your models here.
 
 
@@ -14,6 +15,10 @@ class Product(models.Model):
     category          = models.ForeignKey(Category, on_delete=models.CASCADE) # deletion of category will delete products
     created_date      = models.DateTimeField(auto_now_add=True)
     modified_date     = models.DateTimeField(auto_now=True)
+
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
