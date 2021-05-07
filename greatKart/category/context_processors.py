fcf372dfb59f6cf404d47eabd454784b8b0fe107
ashish807 <ashish.adhikari727@gmail.com>
2021-05-07 13:03:54 +0545

    user authetication done

--- greatKart/category/context_processors.py
+++ greatKart/category/context_processors.py
@@ -3,4 +3,38 @@ from .models import Category
 def menu_links(request):
     links = Category.objects.all()
     return dict(links=links)
-    
+
+
+# def navfoot(request):
+#     navfoot = NavFoot.objects.all()
+#     footerhyperlink =FooterRegistrationHyperLink.objects.all()
+#     aaccrememimg = AccreMemImges.objects.all()
+
+#     return dict(navfoot=navfoot, footerhyperlink=footerhyperlink, aaccrememimg=accreMemImges)
+# {%for nav in navfoot %}
+#     {{foot.image_top}}
+
+# {%for hyperlink in footerhyperlink%}
+#     {{hyperlink.hyperlink}}
+# class AccreMemImges():
+#     image_accre_meme = models.ImageField(upload_to="...")
+
+# class FooterRegistrationHyperLink():
+#     title=models.TextFeild(max_length=500)
+#     hyperlink =models.TextFeild(max_length=500)
+
+# class NavFoot(models.Model):
+#     image_top = models.ImageField(upload_to="...")
+#     book_a_consulattion_hyperlink = models.TextFeild(max_length=500)
+#     occupation_hyperlink = models.TextFeild(max_length=500)
+#     accredentation_member_Title = models.CharFeild(max_length=100)
+#     image_nepal=models.ImageField(upload_to="...")
+#     image_australia=models.ImageField(upload_to="...")
+#     face_nepal=models.TextFeild(max_length=500)
+#     insta_nepl=models.TextFeild(max_length=500)
+#     face_aus=models.TextFeild(max_length=500)
+#     insta_aus=models.TextFeild(max_length=500)
+#     nepal_address=models.TextFeild(max_length=500)
+#     aust=models.TextFeild(max_length=500)
+#     connect_with_us=models.TextFeild(max_length=500)
+
--- greatKart/greatKart/settings.py
+++ greatKart/greatKart/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
                 'carts.context_processors.counter',
+            
             ],
         },
     },
@@ -136,6 +137,14 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS ={
     messages.ERROR: 'danger',
 }
+
+#SMTP CONFIGUTATION
+
+EMAIL_HOST =  'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'aacs807@gmail.com'
+EMAIL_HOST_PASSWORD = 'icanbenumber1'
+EMAIL_USE_TLS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
