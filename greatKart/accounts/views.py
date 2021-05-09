0455c1d1a85560da3469706855b480fe2240d661
ashish807 <ashish.adhikari727@gmail.com>
2021-05-09 20:52:18 +0545

    Ye wala Branch dekh na lcture 15 k liye

--- greatKart/accounts/views.py
+++ greatKart/accounts/views.py
@@ -14,6 +14,7 @@ from django.utils.encoding import force_text
 
 from carts.models import Cart, CartItem
 from carts.views import _cart_item
+import requests
 # Create your views here.
 
 
@@ -57,8 +58,6 @@ def login(request):
     if request.method =='POST':
         email = request.POST['email']
         password = request.POST['password']
-
-
         user = auth.authenticate(email=email, password=password)
   
         if user is not None:
@@ -68,16 +67,49 @@ def login(request):
                 is_cart_Item = CartItem.objects.filter(cart=cart).exists()
                 if is_cart_Item:
                     cart_item = CartItem.objects.filter(cart=cart)
+                    product_variation=[]
+
                     for item in cart_item:
-                        item.user = user
-                        item.save()
+                        variation = item.variation.all()
+                        product_variation.append(list(variation))
+                    
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list =[]
+                    id=[]
+                    for item in cart_item:
+                        existing_variation = item.variation.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+                    
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id= id[index]
+                            item =CartItem.objects.get(id=item_id)
+                            item.quantity+=1
+                            item.user=user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
             except:
                 pass
 
             
             auth.login(request, user)
             messages.success(request, 'You are now looged in')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                params =dict(x.split('=') for x in query.split('&'))
+
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'Invalid Login')
             return redirect('login')
--- greatKart/carts/context_processors.py
+++ greatKart/carts/context_processors.py
@@ -7,8 +7,12 @@ def counter(request):
     
     else:
         try:
-            cart = Cart.objects.filter(cart_id=_cart_item(request))
-            cart_items = CartItem.objects.all().filter(cart=cart[:1])
+            
+            if request.user.is_authenticated:
+                cart_items = CartItem.objects.all().filter(user=request.user)
+            else:
+                cart = Cart.objects.filter(cart_id=_cart_item(request))
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
                 cart_count+=cart_item.quantity 
         except Cart.DoesNotExist:
