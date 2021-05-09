a4f66894c153b7edd077ee5dbf687df0e85a0050
ashish807 <ashish.adhikari727@gmail.com>
2021-05-10 00:53:20 +0545

    Sala ye wala ne to dimag kharab kardiya yr sala phir se database bana na pada fuck this one man

--- greatKart/greatKart/settings.py
+++ greatKart/greatKart/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'carts',
+    'orders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ greatKart/orders/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from carts.models import CartItem
+from .forms import OrderForm
+from .models import Order
+import datetime
+# Create your views here.
+
+
+
+def payment(request):
+    return render(request, 'orders/payment.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def place_order(request, total=0, quantity=0):
+    current_user = request.user
+
+    cart_items = CartItem.objects.filter(user=current_user)
+
+    cart_count = cart_items.count()
+
+    if cart_count<=0:
+        return redirect('store')
+
+    for cart_item in cart_items:
+        total+=(cart_item.product.price*cart_item.quantity)
+        quantity+=cart_item.quantity
+    tax = (2*total)/100
+    grand_total = total +tax
+
+    if request.method == "POST":
+        form =OrderForm(request.POST)
+
+        if form.is_valid():
+            data =Order()
+            data.user=current_user
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone = form.cleaned_data['phone']
+            data.email = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total =grand_total
+            data.tax=tax
+            data.ip=request.META.get('REMOTE_ADDR')
+            data.save()
+
+            #Generate Order number
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d =datetime.date(yr,mt,dt)
+            current_date = d.strftime("%Y%m%d")
+            order_number = current_date + str(data.id)
+            data.order_number =order_number
+            data.save()
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            
+            context = {
+                'order':order,
+                'cart_items':cart_items,
+                'total':total,
+                'tax':tax,
+                'grand_total':grand_total,
+            }
+            return render(request,'orders/payment.html', context)
+        else:
+            return redirect('checkout')
+            
+
+
+
+
